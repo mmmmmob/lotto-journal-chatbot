31db8e2df6478d777a63463653de380703c3a649
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2026-05-08 22:01:20 +0700

    chore: remove unused packages

--- lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('lint-staged').Config} */
-const config = {
-  '**/*.{js,jsx,ts,tsx}': ['pnpm exec prettier --write', 'pnpm exec eslint --fix --max-warnings=0'],
-  '**/*.{json,md,mdx,css,scss,html,yml,yaml}': ['pnpm exec prettier --write'],
-};
-
-export default config;
